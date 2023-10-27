991bbbb7d70d75917efb4ab016ed5c5c6420e587
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-10-27 11:52:16 -0600

    . e Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -6,4 +6,6 @@ HERE=$(cygpath -w $(pwd))
 
 g-cli vipc -- -v "20.0 (64-bit)" -t 1200 "Approval Testing.vipc"
 #g-cli caraya -- -s . -x "UnitTestReport.xml"
+# begin-snippet: test_tests
 g-cli vitester -- -r "UnitTestReport.xml" "Approval Testing.lvproj"
+# end-snippet
