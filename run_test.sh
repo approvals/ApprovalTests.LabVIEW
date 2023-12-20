327bac9deb62cf5b861910512c21ef4161e9c5b6
Samuel Taggart <sam@sasworkshops.com>
2023-12-20 08:27:54 -0700

    . r moved more stuff around.

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-
+mkdir reports
 set -euo pipefail
 
 HERE=$(cygpath -w $(pwd))
@@ -7,7 +7,7 @@ HERE=$(cygpath -w $(pwd))
 g-cli vipc -- -v "20.0 (64-bit)" -t 1200 "Approval Testing.vipc"
 # begin-snippet: run_tests
 echo "Running Caraya Tests" # needed because caray tool is not very verbose.
-g-cli caraya -- -s "Tests\\Caraya.Tests\\Caraya Extension Test Suite\\Caraya Extension Test Suite.lvclass" -x "Caraya.UnitTestReport.xml"
+g-cli caraya -- -s "Tests\\Caraya.Tests\\Caraya Extension Test Suite\\Caraya Extension Test Suite.lvclass" -x "reports\\Caraya.UnitTestReport.xml"
 # Temporarily disable VITester.
-g-cli vitester -- -r "VItester.UnitTestReport.xml" "Approval Testing.lvproj"
+g-cli vitester -- -r "reports\\VItester.UnitTestReport.xml" "Approval Testing.lvproj"
 # end-snippet
