af11076c6882230188fed7d5d2c65bf04b53fe02
Samuel Taggart <sam@sasworkshops.com>
2024-02-21 15:34:23 -0700

    ! E Updated CI to build package.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+VERSION="2.0.0"
+rm build/*.vip 2>/dev/null
+set -euo pipefail
+
+echo "Building $VERSION.$1"
+
+HERE=$(cygpath -w $(pwd))
+SECONDS=0
+g-cli clearlvcache
+g-cli vipc -- -v "20.0 (64-bit)" -t 1200 "approvals-dev.vipc"
+g-cli vipb -- -b "Approval Tests.vipb" -v "$VERSION.$1" -r "release-notes.txt"
+echo "Script Time: $SECONDS"
