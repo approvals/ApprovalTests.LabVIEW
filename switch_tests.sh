3ca9d1ce989f9dff6562c22a7aca8dc0239bcefc
Samuel Taggart <sam@sasworkshops.com>
2024-02-21 18:04:10 -0700

    . e Updated the switch tests script.

--- switch_tests.sh
+++ switch_tests.sh
@@ -3,10 +3,13 @@ set -euo pipefail
 SECONDS=0
 
 g-cli clearlvcache
+# replace each extension
 g-cli switcheroo -- -t "Tests\\Caraya.Tests" -o "Source\\Extensions\\Caraya.Extension\\Caraya.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_Caraya.Extension.lvlib"
 g-cli switcheroo -- -t "Tests\\LUnit.Tests" -o "Source\\Extensions\\LUnit.Extension\\LUnit.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_LUnit.Extension.lvlib"
 g-cli switcheroo -- -t "Tests\\VITester.Tests" -o "Source\\Extensions\\Caraya.Extension\\VITester.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_VITester.Extension.lvlib"
 g-cli switcheroo -- -t "Tests\\Scrubber.Tests" -o "Source\\Extensions\\Caraya.Extension\\Caraya.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_Caraya.Extension.lvlib"
-g-cli switcheroo -- -t "Tests\\Scrubber.Tests" -o "Source\\Approval Tests\\Approval Tests.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Approval Tests\\lib_Approval Tests.lvlib"
+# replace approvals in all in case they are calling approvals directly - scrubber tests definitely are.
+g-cli switcheroo -- -t "Tests" -o "Source\\Approval Tests\\Approval Tests.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Approval Tests\\lib_Approval Tests.lvlib"
+
 
 echo "Total Script Time $SECONDS"
