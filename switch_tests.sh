1461040901294dc86bcd2e17f2ac59887a927f71
Samuel Taggart <sam@sasworkshops.com>
2024-02-23 09:36:07 -0700

    . e Updated bad path - CI should work now?

--- switch_tests.sh
+++ switch_tests.sh
@@ -6,7 +6,7 @@ g-cli clearlvcache
 # replace each extension
 g-cli switcheroo -- -t "Tests" -o "Source\\Extensions\\Caraya.Extension\\Caraya.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_Caraya.Extension.lvlib"
 g-cli switcheroo -- -t "Tests" -o "Source\\Extensions\\LUnit.Extension\\LUnit.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_LUnit.Extension.lvlib"
-g-cli switcheroo -- -t "Tests" -o "Source\\Extensions\\Caraya.Extension\\VITester.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_VITester.Extension.lvlib"
+g-cli switcheroo -- -t "Tests" -o "Source\\Extensions\\VITester.Extension\\VITester.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_VITester.Extension.lvlib"
 g-cli switcheroo -- -t "Tests" -o "Source\\Approval Tests\\Approval Tests.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Approval Tests\\lib_Approval Tests.lvlib"
 
 
