039c7ffde188db4fc9805f26d4b8d23328b8da5f
Samuel Taggart <sam@sasworkshops.com>
2025-10-04 21:44:33 -0600

    . e Removed leading _ from scripts. No longer needed.

---
+++ scripts/ci.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+set -euo pipefail
+
+git submodule init
+git submodule update
+
+./scripts/run_unittests.sh
+./scripts/build.sh $1
+./scripts/install.sh $1
+./scripts/create_install_tests.sh
+./scripts/run_install_tests.sh
