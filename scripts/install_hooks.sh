11a9eb03353e9e084a058bd87b857033f8edccef
Samuel Taggart <sam@sasworkshops.com>
2025-10-10 10:49:59 -0600

    . e Added some color to run_unittests.sh output and added git hooks.

---
+++ scripts/install_hooks.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+#intended to be run from project root
+
+cp -r scripts/hooks .git/hooks
+
--- scripts/run_unittests.sh
+++ scripts/run_unittests.sh
@@ -5,12 +5,16 @@ rm "reports\\LUnit.UnitTestReport.xml" 2>/dev/null
 rm "reports\\VITester.UnitTestReport.xml" 2>/dev/null
 rm "reports\\Scrubber.UnitTestReport.xml" 2>/dev/null
 
+green=$(tput setaf2)
+red=$(tput setaf1)
+reset=$(tput sgr0)
+bold=$(tput bold)
+
 set -euo pipefail
 
 HERE=$(cygpath -w $(pwd))
 
 g-cli vipc -- -v "${LV_VERSION:-"20.0 (64-bit)"}" -t 1200 "approvals-dev.vipc"
-# begin-snippet: run_tests
 g-cli lunit -- -r "reports\\LUnit.UnitTestReport.xml" "Approval Testing.lvproj"
 g-cli vitester -- -r "reports\\VITester.UnitTestReport.xml" "Tests\\Extension Tests.lvproj"
 SECONDS=0
@@ -20,5 +24,7 @@ echo "Test Time: $SECONDS"
 SECONDS=0
 echo "Running Error Propagation Tests"
 g-cli caraya -- -s "Tests\\Error.Propagation.Tests\\Error.Propagation.Tests.lvclass" -x "reports\\Error.Propagation.UnitTestReport.xml"
-echo "Test Time: $SECONDS"
-# end-snippet
+
+echo "${bold}${green}Result Pass${reset}" 
+echo "${bold}Test Time: $SECONDS${reset}"
+
