b9f487d03a60cd2c12113ab1098e07384985c3a4
Sam Taggart <sam@sasworkshops.com>
2026-03-06 21:41:30 -0800

    . e Fixed VIPM activation command. should work now.

--- scripts/install_VIPM.sh
+++ scripts/install_VIPM.sh
@@ -11,5 +11,5 @@ wget -O /tmp/vipm.deb \
     https://packages.jki.net/vipm/preview/vipm_latest_preview_amd64.deb \
     && dpkg -i /tmp/vipm.deb \
     && rm /tmp/vipm.deb
-vipm activate
+vipm activate --serial-number "${VIPM_SERIAL_NUMBER}" --name "${VIPM_FULL_NAME}" --email "${VIPM_EMAIL}"
 vipm about
