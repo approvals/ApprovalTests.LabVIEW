6397d900778ebadce637ecace91cdd6a35313e16
Samuel Taggart <sam@sasworkshops.com>
2026-03-13 22:34:30 -0600

    . e added code to create needed directory

--- scripts/install_VIPM.sh
+++ scripts/install_VIPM.sh
@@ -11,6 +11,7 @@ wget -q -O /tmp/vipm.deb \
     https://packages.jki.net/vipm/preview/vipm_latest_preview_amd64.deb \
     && dpkg -i /tmp/vipm.deb \
     && rm /tmp/vipm.deb
+mkdir -p /C/ProgramData/JKI/VIPM/databases
 vipm activate --serial-number "${VIPM_SERIAL_NUMBER}" --name "${VIPM_FULL_NAME}" --email "${VIPM_EMAIL}"
 vipm about
 vipm_lv_version="${VIPM_LV_VERSION:-2026}"
