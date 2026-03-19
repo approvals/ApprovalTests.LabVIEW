3e30ab29534c2e0995f9e5376abce5f8556052ee
Sam Taggart <sam@sasworkshops.com>
2026-03-18 22:29:35 -0600

    updated build script and removed LVbuildspec - shouldn't have included that.

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,5 +19,12 @@ g-cli clearlvcache
 g-cli quitLabVIEW
 sleep 2s
 vipm install "approvals-dev.vipc"
-g-cli vipb -- -b "Approval Tests.vipb" -v "$VERSION.$1" -rn "release-notes.txt"
+curl -L -o xml.zip https://downloads.sourceforge.net/project/xmlstar/xmlstarlet/1.6.1/xmlstarlet-1.6.1-win32.zip
+unzip xml.zip
+./xmlstarlet-1.6.1/xml ed --inplace -u VI_Package_Builder_Settings/Advanced_Settings/Description/Release_Notes --value "$(dos2unix release-notes.txt && cat release-notes.txt)" "$VIPB_PATH"
+./xmlstarlet-1.6.1/xml ed --inplace -u VI_Package_Builder_Settings/Library_General_Settings/Library_Version --value "${full_version}" "$VIPB_PATH"
+rm -r xml.starlet-1.6.1
+rm xml.zip
+vipm build "Approval Tests.vipb
+
 echo "Script Time: $SECONDS"
