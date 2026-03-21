582b5dc26693c3e4c6fed0a1d9ce2e85f6990cff
Sam Taggart <sam@sasworkshops.com>
2026-03-20 21:14:42 -0600

    More troubleshooting

--- scripts/run_unittests.sh
+++ scripts/run_unittests.sh
@@ -32,14 +32,27 @@ set -euo pipefail
 
 
 vipm install "approvals-dev.vipc" || fail
+
+echo "nisvcloc PID before suite 1: $(pgrep nisvcloc)"
+
+
 SECONDS=0
 echo "Running Caraya Extension Tests" # needed because caray tool is not very verbose.
 g-cli --timeout 300000 Caraya -- -s "Tests${SEP}Caraya.Tests${SEP}Caraya Extension Tests${SEP}Caraya Extension Tests.lvclass" -x "reports${SEP}Caraya.UnitTestReport.xml" || fail
 echo "Test Time: $SECONDS"
+
+
+echo "nisvcloc PID before suite 2: $(pgrep nisvcloc)"
+
 SECONDS=0
 echo "Running Error Propagation Tests"
 g-cli --timeout 300000 Caraya -- -s "Tests${SEP}Error.Propagation.Tests${SEP}Error.Propagation.Tests.lvclass" -x "reports${SEP}Error.Propagation.UnitTestReport.xml" || fail
 echo "Test Time: $SECONDS"
+
+echo "nisvcloc PID before suite 3: $(pgrep nisvcloc)"
+
+
+
 g-cli --timeout 300000 lunit -- -r "reports${SEP}LUnit.UnitTestReport.xml" "Approval Testing.lvproj" || fail
 echo "${bold}${green}PASS${reset}" 
 
