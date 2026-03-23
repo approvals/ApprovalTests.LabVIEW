d9750d5b7deb75ad4dac076d6156fad35d115a20
Sam Taggart <sam@sasworkshops.com>
2026-03-23 08:51:35 -0600

    . e Added script to set ini file values.

---
+++ scripts/prep_ini_file.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+INI_DIR="$HOME/natinst/.config/LabVIEW-2026"
+INI_FILES=(
+    "$INI_DIR/labview.ini"
+    "$INI_DIR/labview64.ini"
+    "$INI_DIR/labviewprofull.ini"
+)
+
+SECTION="LabVIEW"
+
+KEYS=(
+    "NIERShowFatalDialog"
+    "NIERFatalAutoSend"
+    "NIERNonFatalAutoSend"
+    "NIERShowNonFatalDialogOnExit"
+    "NIERSendDialogClose"
+    "autoerr"
+    "DWarnDialog"
+    "AutoSaveEnabled"
+    "AllowMultipleInstances"
+    "promoteDWarnInternals"
+    "ShowAIVIsInErrorWindow"
+)
+
+VALUES=(
+    "0"
+    "true"
+    "true"
+    "false"
+    "true"
+    "3"
+    "false"
+    "false"
+    "true"
+    "false"
+    "true"
+)
+
+set_ini_value() {
+    local file="$1"
+    local key="$2"
+    local value="$3"
+
+    if grep -q "^${key}" "$file"; then
+        sed -i "s|^${key}.*|${key} = ${value}|" "$file"
+    else
+        sed -i "/^\[${SECTION}\]/a ${key} = ${value}" "$file"
+    fi
+}
+
+mkdir -p "$INI_DIR"
+
+for ini_file in "${INI_FILES[@]}"; do
+    echo "Configuring $ini_file..."
+
+    touch "$ini_file"
+
+    if ! grep -q "^\[${SECTION}\]" "$ini_file"; then
+        printf '\n[%s]\n' "$SECTION" >> "$ini_file"
+    fi
+
+    for i in "${!KEYS[@]}"; do
+        set_ini_value "$ini_file" "${KEYS[$i]}" "${VALUES[$i]}"
+    done
+
+    echo "Done: $ini_file"
+done
