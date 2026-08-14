d67deb7238cb5a4fdda08d2454e701dfce7512a7
Sam Taggart <sam@sasworkshops.com>
2026-08-13 20:41:31 -0600

    . e split up the create and run install tests. Added some echos for debugging. and fixed few paths. Should probably take the tests and break things down so test reports mimic preinstall tests, but later. Get working first.

--- scripts/run_install_tests.sh
+++ scripts/run_install_tests.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 mkdir reports 2> /dev/null
-rm "reports\\Caraya.InstallTestReport.xml" 2>/dev/null
-rm "reports\\LUnit.InstallTestReport.xml" 2>/dev/null
+rm "reports/Caraya.InstallTestReport.xml" 2>/dev/null
+rm "reports/LUnit.InstallTestReport.xml" 2>/dev/null
 
 set -euo pipefail
 
 g-cli --kill --kill-timeout 5000 lunit -- -r "reports/LUnit.InstallTestReport.xml" "InstallTests"
 SECONDS=0
-echo "Running Caraya Extension Tests" # needed because caray tool is not very verbose.
+echo "Running Caraya Extension Tests" # needed because Caraya tool is not very verbose.
 g-cli --kill --kill-timeout 5000 Caraya -- -s "InstallTests/Caraya.Tests/Caraya Extension Tests/Caraya Extension Tests.lvclass" -x "reports/Caraya.InstallTestReport.xml"
 echo "Test Time: $SECONDS"
