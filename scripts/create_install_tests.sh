174a28aefe28ea717ea19da7a4749d2e57cda293
Sam Taggart <sam@sasworkshops.com>
2026-08-13 19:45:10 -0600

    . e Added code to run install tests.

--- scripts/create_install_tests.sh
+++ scripts/create_install_tests.sh
@@ -3,16 +3,11 @@
 rm -r InstallTests 2>/dev/null
 
 set -euo pipefail
-SECONDS=0
-
-g-cli --kill clearlvcache
 
 cp -r Tests InstallTests
 
-# replace each extension
-g-cli switcheroo -- -t "InstallTests" -o "Source\\Extensions\\Caraya.Extension\\Caraya.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_Caraya.Extension.lvlib"
-g-cli switcheroo -- -t "InstallTests" -o "Source\\Extensions\\LUnit.Extension\\LUnit.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_LUnit.Extension.lvlib"
-g-cli switcheroo --kill -- -t "InstallTests" -o "Source\\Approval Tests\\Approval Tests.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Approval Tests\\lib_Approval Tests.lvlib"
+# replace each extension and core library
+g-cli --kill --kill-timeout 5000 switcheroo -- -t "InstallTests" -o "Source/Extensions/Caraya.Extension/Caraya.Extension.lvlib" -r "<vilib>/SAS/Approval Tests/Extensions\\lib_Caraya.Extension.lvlib"
+g-cli --kill --kill-timeout 5000 switcheroo -- -t "InstallTests" -o "Source\\Extensions\\LUnit.Extension\\LUnit.Extension.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Extensions\\lib_LUnit.Extension.lvlib"
+g-cli --kill --kill-timeout 5000 switcheroo --kill -- -t "InstallTests" -o "Source\\Approval Tests\\Approval Tests.lvlib" -r "<vilib>\\SAS\\Approval Tests\\Approval Tests\\lib_Approval Tests.lvlib"
 
-
-echo "Total Script Time $SECONDS"
--- scripts/run_install_tests.sh
+++ scripts/run_install_tests.sh
@@ -5,11 +5,8 @@ rm "reports\\LUnit.InstallTestReport.xml" 2>/dev/null
 
 set -euo pipefail
 
-HERE=$(cygpath -w $(pwd))
-g-cli vipc -- -v "${LV_VERSION:-"20.0 (64-bit)"}" -t 1200 "approvals-dev.vipc"
-# if above fails set LV_VERSION ENV variable. If you have the vip tool. then `g-cli vip -- --list` will list all available version strings. 
-g-cli lunit -- -r "reports\\LUnit.InstallTestReport.xml" "InstallTests"
+g-cli --kill --kill-timeout 5000 lunit -- -r "reports/LUnit.InstallTestReport.xml" "InstallTests"
 SECONDS=0
 echo "Running Caraya Extension Tests" # needed because caray tool is not very verbose.
-g-cli Caraya -- -s "InstallTests\\Caraya.Tests\\Caraya Extension Tests\\Caraya Extension Tests.lvclass" -x "reports\\Caraya.InstallTestReport.xml"
+g-cli --kill --kill-timeout 5000 Caraya -- -s "InstallTests/Caraya.Tests/Caraya Extension Tests/Caraya Extension Tests.lvclass" -x "reports/Caraya.InstallTestReport.xml"
 echo "Test Time: $SECONDS"
