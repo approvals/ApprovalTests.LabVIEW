0227d670da59f2b21413f5482a9cd25354249daa
Sam Taggart <sam@sasworkshops.com>
2026-08-20 09:01:07 -0600

    . r Cleaned up tests. Shortened var names. Removed commented code.

--- scripts/run_install_tests.sh
+++ scripts/run_install_tests.sh
@@ -1,24 +1,11 @@
 #! /usr/bin/env bash
-# Old Code
-# mkdir reports 2> /dev/null
-# rm "reports/Caraya.InstallTestReport.xml" 2>/dev/null
-# rm "reports/LUnit.InstallTestReport.xml" 2>/dev/null
-
-# set -euo pipefail
-
-# # g-cli --kill --kill-timeout 5000 lunit -- -r "reports/LUnit.InstallTestReport.xml" "InstallTests"
-# # SECONDS=0
-# # echo "Running Caraya Extension Tests" # needed because Caraya tool is not very verbose.
-# # g-cli --kill --kill-timeout 5000 Caraya -- -s "InstallTests/Caraya.Tests/Caraya Extension Tests/Caraya Extension Tests.lvclass" -x "reports/Caraya.InstallTestReport.xml"
-# # echo "Test Time: $SECONDS"
 
 REPORTS_DIR="reports"
 
-CARAYA_POSTINSTALL_REPORT="$REPORTS_DIR/Caraya.PostInstall.UnitTestReport.xml"
-LUNIT_POSTINSTALL_REPORT="$REPORTS_DIR/LUnit.PostInstall.UnitTestReport.xml"
-ERROR_PROPAGATION_POSTINSTALL_REPORT="$REPORTS_DIR/Error.PostInstall.Propagation.UnitTestReport.xml"
-CORE_LIBRARY_POSTINSTALL_REPORT="$REPORTS_DIR/CoreLibrary.PostInstall.UnitTestReport.xml"
-
+CARAYA_REPORT="$REPORTS_DIR/Caraya.PostInstall.UnitTestReport.xml"
+LUNIT_REPORT="$REPORTS_DIR/LUnit.PostInstall.UnitTestReport.xml"
+ERROR_PROPAGATION_REPORT="$REPORTS_DIR/Error.PostInstall.Propagation.UnitTestReport.xml"
+CORE_LIBRARY_REPORT="$REPORTS_DIR/CoreLibrary.PostInstall.UnitTestReport.xml"
 
 mkdir "$REPORTS_DIR" 2> /dev/null
 echo "Running Unit Tests"
@@ -27,19 +14,16 @@ if [ "${1:-}" == "kill" ]; then
   kill="--kill --kill-timeout 5000"
 fi
 
-
-rm "${CARAYA_POSTINSTALL_REPORT}" 2>/dev/null
-rm "${LUNIT_POSTINSTALL_REPORT}" 2>/dev/null
-rm "${ERROR_PROPAGATION_POSTINSTALL_REPORT}" 2>/dev/null
-rm "${CORE_LIBRARY_POSTINSTALL_REPORT}" 2>/dev/null
+rm "${CARAYA_REPORT}" 2>/dev/null
+rm "${LUNIT_REPORT}" 2>/dev/null
+rm "${ERROR_PROPAGATION_REPORT}" 2>/dev/null
+rm "${CORE_LIBRARY_REPORT}" 2>/dev/null
 
 export TERM=${TERM:-xterm}
 green=$(tput setaf 2)
 red=$(tput setaf 1)
 reset=$(tput sgr0)
 bold=$(tput bold)
-
-
 
 fail() {
     echo "${bold}${red}FAIL${reset}"
@@ -48,23 +32,18 @@ fail() {
 # set -x
 set -euo pipefail
 
+g-cli ${kill} --timeout 180000 lunit -- -r "${CORE_LIBRARY_REPORT}" "Tests/Core Library Tests.lvproj" || fail
 
-
-g-cli ${kill} --timeout 180000 lunit -- -r "${CORE_LIBRARY_POSTINSTALL_REPORT}" "Tests/Core Library Tests.lvproj" || fail
-
-g-cli ${kill} --timeout 180000 lunit -- -r "${LUNIT_POSTINSTALL_REPORT}" "Tests/Extension Tests.lvproj" || fail
-
-
+g-cli ${kill} --timeout 180000 lunit -- -r "${LUNIT_REPORT}" "Tests/Extension Tests.lvproj" || fail
 
 SECONDS=0
 echo "Running Caraya Extension Tests" # needed because caray tool is not very verbose.
-g-cli ${kill} --timeout 180000 Caraya -- -s "Tests/Caraya.Tests/Caraya Extension Tests/Caraya Extension Tests.lvclass" -x "${CARAYA_POSTINSTALL_REPORT}" || fail
+g-cli ${kill} --timeout 180000 Caraya -- -s "Tests/Caraya.Tests/Caraya Extension Tests/Caraya Extension Tests.lvclass" -x "${CARAYA_REPORT}" || fail
 echo "Test Time: $SECONDS"
-
 
 SECONDS=0
 echo "Running Error Propagation Tests"
-g-cli ${kill} --timeout 180000 Caraya -- -s "Tests/Error.Propagation.Tests/Error.Propagation.Tests.lvclass" -x "${ERROR_PROPAGATION_POSTINSTALL_REPORT}" || fail
+g-cli ${kill} --timeout 180000 Caraya -- -s "Tests/Error.Propagation.Tests/Error.Propagation.Tests.lvclass" -x "${ERROR_PROPAGATION_REPORT}" || fail
 echo "Test Time: $SECONDS"
 
 echo "${bold}${green}PASS${reset}" 
--- scripts/run_unittests.sh
+++ scripts/run_unittests.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 REPORTS_DIR="reports"
 
-CARAYA_PREBUILD_REPORT="$REPORTS_DIR/Caraya.PreBuild.UnitTestReport.xml"
-LUNIT_PREBUILD_REPORT="$REPORTS_DIR/LUnit.PreBuild.UnitTestReport.xml"
-ERROR_PROPAGATION_PREBUILD_REPORT="$REPORTS_DIR/ErrorPropagation.PreBuild.UnitTestReport.xml"
-CORE_LIBRARY_PREBUILD_REPORT="$REPORTS_DIR/CoreLibrary.PreBuild.UnitTestReport.xml"
+CARAYA_REPORT="$REPORTS_DIR/Caraya.PreBuild.UnitTestReport.xml"
+LUNIT_REPORT="$REPORTS_DIR/LUnit.PreBuild.UnitTestReport.xml"
+ERROR_PROPAGATION_REPORT="$REPORTS_DIR/ErrorPropagation.PreBuild.UnitTestReport.xml"
+CORE_LIBRARY_REPORT="$REPORTS_DIR/CoreLibrary.PreBuild.UnitTestReport.xml"
 
 
 mkdir "$REPORTS_DIR" 2> /dev/null
@@ -15,10 +15,10 @@ if [ "${1:-}" == "kill" ]; then
 fi
 
 
-rm "$CARAYA_PREBUILD_REPORT" 2>/dev/null
-rm "$LUNIT_PREBUILD_REPORT" 2>/dev/null
-rm "$ERROR_PROPAGATION_PREBUILD_REPORT" 2>/dev/null
-rm "$CORE_LIBRARY_PREBUILD_REPORT" 2>/dev/null
+rm "$CARAYA_REPORT" 2>/dev/null
+rm "$LUNIT_REPORT" 2>/dev/null
+rm "$ERROR_PROPAGATION_REPORT" 2>/dev/null
+rm "$CORE_LIBRARY_REPORT" 2>/dev/null
 
 export TERM=${TERM:-xterm}
 green=$(tput setaf 2)
@@ -37,21 +37,21 @@ set -euo pipefail
 
 
 
-g-cli ${kill} --timeout 180000 lunit -- -r "${CORE_LIBRARY_PREBUILD_REPORT}" "Tests/Core Library Tests.lvproj" || fail
+g-cli ${kill} --timeout 180000 lunit -- -r "${CORE_LIBRARY_REPORT}" "Tests/Core Library Tests.lvproj" || fail
 
-g-cli ${kill} --timeout 180000 lunit -- -r "${LUNIT_PREBUILD_REPORT}" "Tests/Extension Tests.lvproj" || fail
+g-cli ${kill} --timeout 180000 lunit -- -r "${LUNIT_REPORT}" "Tests/Extension Tests.lvproj" || fail
 
 
 
 SECONDS=0
 echo "Running Caraya Extension Tests" # needed because caray tool is not very verbose.
-g-cli ${kill} --timeout 180000 Caraya -- -s "Tests/Caraya.Tests/Caraya Extension Tests/Caraya Extension Tests.lvclass" -x "${CARAYA_PREBUILD_REPORT}" || fail
+g-cli ${kill} --timeout 180000 Caraya -- -s "Tests/Caraya.Tests/Caraya Extension Tests/Caraya Extension Tests.lvclass" -x "${CARAYA_REPORT}" || fail
 echo "Test Time: $SECONDS"
 
 
 SECONDS=0
 echo "Running Error Propagation Tests"
-g-cli ${kill} --timeout 180000 Caraya -- -s "Tests/Error.Propagation.Tests/Error.Propagation.Tests.lvclass" -x "${ERROR_PROPAGATION_PREBUILD_REPORT}" || fail
+g-cli ${kill} --timeout 180000 Caraya -- -s "Tests/Error.Propagation.Tests/Error.Propagation.Tests.lvclass" -x "${ERROR_PROPAGATION_REPORT}" || fail
 echo "Test Time: $SECONDS"
 
 echo "${bold}${green}PASS${reset}" 
